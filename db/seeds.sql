dfc498ab4a50dd1a042db3bc9561acc36dbc3224
rickdeakins <rickdeakinsjr@gmail.com>
2024-04-08 22:47:38 -0400

    Initial file containing seeds for new database and tables. Initial push so further seed data will be submitted in the future.

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+NSERT INTO patient_basic (patient_name)
+VALUES ("John Smith"),
+       ("Maria James"),
+       ("Alex White");
+
+INSERT INTO patient_financialsummary (patient_id, number_of_appointments)
+VALUES (1, "3"),
+       (2, "1"),
+       (3, "7");
