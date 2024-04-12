36b0c0acbed8e8c026b16b11bbc4b65622cd1bb7
rickdeakins <rickdeakinsjr@gmail.com>
2024-04-11 22:25:20 -0400

    Creation of Patient model file. Patient table parameters defined.

---
+++ models/Patient.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Patient extends Model {}
+
+Patient.init(
+  {
+    patient_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    patient_name: {
+      type: DataTypes.STRING
+    },
+    patient_guardian: {
+      type: DataTypes.STRING
+    },
+    physical_address: {
+      type: DataTypes.STRING
+    },
+    phone_number: {
+      type: DataTypes.INTEGER
+    },
+    email_address: {
+      type: DataTypes.INTEGER
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'book'
+  }
+);
+
+module.exports = Patient;
