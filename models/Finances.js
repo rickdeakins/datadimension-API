c0ae55beac58a2216371af9c0afb42d553f63abc
rickdeakins <rickdeakinsjr@gmail.com>
2024-04-12 21:21:05 -0400

    Initial implementation of Finances mmodel. patient_financial_summary table defined, required parameters defined.

---
+++ models/Finances.js
@@ -0,0 +1,32 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Finances extends Model {}
+
+Patient.init(
+  {
+    patient_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    patient_name: {
+      type: DataTypes.STRING
+    },
+    number_of_appointments: {
+      type: DataTypes.INTEGER
+    },
+    total_charges: {
+      type: DataTypes.DECIMAL
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'finances'
+  }
+);
+
+module.exports = Finances;
