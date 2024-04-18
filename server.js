70861e3e0765b6c75dc17e890ae2a695511df5be
rickdeakins <rickdeakinsjr@gmail.com>
2024-04-17 22:00:20 -0400

    Additional command to use route endpoints.

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const sequelize = require('./config/connection');
+const routes = require('./routes');
+const sequelize = require('./config/connections');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
 sequelize.sync().then(() => {
   app.listen(PORT, () => console.log('Now listening'));
